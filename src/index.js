d493ff11913700ffb280c27b4a6a81a21c991346
zama <zamaz@workonline.africa>
2026-06-11 09:08:52 +0200

    updating project with user experience

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ function generatePoem(event) {
   let prompt = `User instructions: Generate a french poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating"> ⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
