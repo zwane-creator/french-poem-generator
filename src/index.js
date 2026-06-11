a8eff56b405701539d4d3c1a1b12ddaccb1bc294
zama <zamaz@workonline.africa>
2026-06-11 09:21:44 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "dca402bf6e5cod4bb210fdbta88ea4c3";
   let context =
-    "You are a romantic poem expect and love to write short poems. Your mission a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end not at the beginning of the poem on a <br />";
-  let prompt = `User instructions: Generate a Zulu poem about ${instructionsInput.value}`;
+    "You are a romantic poem expect and love to write short love letter. Your mission a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end not at the beginning of the poem on a <br />";
+  let prompt = `User instructions: Generate a Zulu love letter about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
