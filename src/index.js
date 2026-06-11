65990041a250edea6c878ad39417213742d7615f
zama <zamaz@workonline.africa>
2026-06-11 09:17:51 +0200

    fixed generating sentence

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating"> ⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating"> ⏳ Generating a Zulu poem about ${instructionsInput.value}</div>`;
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
