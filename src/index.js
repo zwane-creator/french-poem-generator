10dca4f6c120bd010288fec54bc8b4613809f9c2
zama <zamaz@workonline.africa>
2026-06-12 07:12:57 +0200

    Enter the topic of the English poem/joke/recipe/quote

--- src/index.js
+++ src/index.js
@@ -14,15 +14,15 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "dca402bf6e5cod4bb210fdbta88ea4c3";
   let context =
-    "You are a romantic poem expect and love to write short poems. Your mission a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end not at the beginning of the poem on a <br />";
-  let prompt = `User instructions: Generate a Zulu poem about ${instructionsInput.value}`;
+    "You are a literature expert and love to write short poems, jokes, recipes and quotes. Your mission a 4 line poem/jokes/recipes/quotes and separate each line with a <br />. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end not at the beginning of the poem on a <br />";
+  let prompt = `User instructions: Generate a English poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating"> ⏳ Generating a Zulu poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating"> ⏳ Generating a ${instructionsInput.value}</div>`;
 
-  console.log("Generating poem");
+  console.log("Generating ");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
 
